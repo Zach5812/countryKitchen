d0c7866ed71eaaeab0ac36beb4828ef30ec19fc9
Zach5812 <zachlouk7@gmail.com>
2023-05-19 12:43:29 -0700

    added cors

---
+++ server/configs/recipes.configs.js
@@ -0,0 +1,14 @@
+configs:
+const mongoose = require('mongoose');
+const username = process.env.ATLAS_USERNAME;
+const pw = process.env.ATLAS_PASSWORD;
+const dbName = process.env.DB_Name;
+const uri = `mongodb+srv://${username}:${pw}@cluster0.1idv5d9.mongodb.net/${dbName}?retryWrites=true&w=majority`
+
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
