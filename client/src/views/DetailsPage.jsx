6080a5d6e0887bed6780049be73b299c431b4903
Zach5812 <zachlouk7@gmail.com>
2023-05-22 13:37:26 -0700

    details v1.04

--- client/src/views/DetailsPage.jsx
+++ client/src/views/DetailsPage.jsx
@@ -5,39 +5,52 @@ import axios from 'axios';
 const DetailsPage = () => {
     const [recipe, setRecipe] = useState();
 
-    const {id} = useParams();
+    const { id } = useParams();
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/recipes/${id}`)
-        .then(response => {
-            setRecipe(response.data)
-        })
-        .catch(error => console.log(error))
+            .then(response => {
+                setRecipe(response.data)
+            })
+            .catch(error => console.log(error))
     }, [id])
 
-  return (
-    <div>{recipe? 
-    <div> <h1>{recipe.title}</h1>
-    <h3>{recipe.description}</h3>
+    return (
+        <div>{recipe ?
+            <div> <h1>{recipe.title}</h1>
+                <h3>{recipe.description}</h3>
 
-    <p>
-        <h3>Ingredients</h3>
-        {recipe.ingredients.map((eachIng, idx)=>(
-            <p>{eachIng.amount}{eachIng.measurement} {eachIng.name}</p>
-        ))}
-    </p>
+                <p>
+                    <h3>Ingredients</h3>
+                    {recipe.ingredients.map((eachIng, idx) => (
+                        <p key={idx}>{eachIng.amount}{eachIng.measurement} {eachIng.name}</p>
+                    ))}
+                </p>
+                <br />
+
+                <p>
+                    <h3>Methods</h3>
+                    {recipe.methods.map((eachMeth, idx) => (
+                        <p key={idx}>{idx + 1}. {eachMeth}</p>
+                    ))}
+                </p>
+                <br />
+                <p>
+                    <h3>Recipe History</h3>
+                    {recipe.story}
+                </p>
 <br />
+                <p>
+                    <h3>Comments</h3>
+                    {recipe.comments.map((eachComment, idx) => (
+                        <p key={idx}>{eachComment.name} <br />{eachComment.comm}{eachComment.rating} </p>
+                    ))}
+                </p>
 
-<p>
-    <h3>Methods</h3>
-    {recipe.methods.map((eachMeth, idx) =>(
-        <p>{idx+1}. {recipe.methods}</p>
-    ))}
-</p>
-    </div>:
-    <p>Not available</p>
-    }</div>
-  )
+            </div> :
+            <p>Not available</p>
+        }</div>
+    )
 }
 
 export default DetailsPage
